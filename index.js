e28c2441ee6c81384c520845cced95755914c1ba
koizumikoizumiiiii <suke009@gmail.com>
2018-01-26 21:20:14 +0000

    はてブテクノロジーカテゴリへのリダイレクト追加

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
+  res.writeHead(302, {
+    'Location': 'http://b.hatena.ne.jp/ctop/it'
+  });
   res.end();
 });
 const port = 8000;
